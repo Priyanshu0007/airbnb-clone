bd68b8fd604e8d03b9f973a04e1edd77b019419f
Priyanshu Gupta <priyanshu2859@gmail.com>
2023-09-19 17:21:16 +0530

    Header added

---
+++ app/search/layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+const layout = ({children}) => {
+  return (
+    <>
+    <div><Header/></div>
+    <main className='relative top-[7.5rem] pt-4 container mx-auto'>
+    {children}
+    </main>
+    </>
+  )
+}
+
+export default layout
