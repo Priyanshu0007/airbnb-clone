881762c7c9c0b1d4eb19d3c67cb451180f58b1ff
Priyanshu Gupta <priyanshu2859@gmail.com>
2023-09-20 19:35:10 +0530

    fetch by get static props

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,5 +1,14 @@
 import { NextResponse } from "next/server";
 import listingData from "../../listingsData.json"
-export async function GET(req){
+// export async function GET(req){
+//     return NextResponse.json(listingData);
+// }
+export async function getStaticProps() {
+    // Instead of fetching your `/api` route you can call the same
+    // function directly in `getStaticProps`
+    const posts = await listingData()
+   
+    // Props returned will be passed to the page component
     return NextResponse.json(listingData);
-}
+  }
+  
